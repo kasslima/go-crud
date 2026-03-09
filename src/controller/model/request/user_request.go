c80481fb92108f5c3218884a68a55ba5b965fbc3
Lucas Ribeiro <lucasribeiro292004@hotmail.com>
2026-03-09 17:47:32 -0300

    feat: UserRequest validation

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,7 +1,7 @@
 package request
 
 type UserRequest struct {
-	Name  string `json:"name" binding:"required"`
+	Name  string `json:"name" binding:"required,min=2,max=100"`
 	Email string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required,min=6"`
 	Age   int8    `json:"age" binding:"required,gte=0,lte=120"`
